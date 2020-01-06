29e0383ba3755f7778d886810483864b61ec7bd0
vardhah <38508679+vardhah@users.noreply.github.com>
2020-01-06 17:24:23 -0600

    Add files via upload

---
+++ ActorNetwork.py
@@ -0,0 +1,64 @@
+import numpy as np
+import math
+from keras.initializers import normal, identity
+from keras.models import model_from_json
+from keras.models import Sequential, Model
+from keras.layers import *
+from keras.optimizers import Adam
+import tensorflow as tf
+import keras.backend as K
+
+#neuron size declaration
+HIDDEN1_UNITS = 300
+HIDDEN2_UNITS = 600
+
+class ActorNetwork(object):
+    def __init__(self, sess, state_size, action_size, BATCH_SIZE, TAU, LEARNING_RATE):
+        self.sess = sess
+        self.BATCH_SIZE = BATCH_SIZE
+        self.TAU = TAU
+        self.LEARNING_RATE = LEARNING_RATE
+        self.action_size = action_size
+
+        K.set_session(sess)
+        
+        print("------------  I am in actor network ------------------------")
+        #Now create the model
+        self.model , self.weights, self.state = self.create_actor_network(state_size, action_size)   
+        self.target_model, self.target_weights, self.target_state = self.create_actor_network(state_size, action_size) 
+        self.action_gradient = tf.placeholder(tf.float32,[None, action_size])
+        self.params_grad = tf.gradients(self.model.output, self.weights, -self.action_gradient)
+        grads = zip(self.params_grad, self.weights)
+        self.optimize = tf.train.AdamOptimizer(LEARNING_RATE).apply_gradients(grads)
+        self.sess.run(tf.initialize_all_variables())
+
+    def train(self, states, action_grads):
+        self.sess.run(self.optimize, feed_dict={
+            self.state: states,
+            self.action_gradient: action_grads
+        })
+
+    def target_train(self):
+        actor_weights = self.model.get_weights()
+        actor_target_weights = self.target_model.get_weights()
+        for i in xrange(len(actor_weights)):
+            actor_target_weights[i] = self.TAU * actor_weights[i] + (1 - self.TAU)* actor_target_weights[i]
+        self.target_model.set_weights(actor_target_weights)
+
+    def create_actor_network(self, state_size,action_dim):
+        print("Now we build the model")
+        
+        S=Input(shape=[state_size])   
+        h0 = Dense(HIDDEN1_UNITS, activation='relu')(S)
+        h1 = Dense(HIDDEN2_UNITS, activation='relu')(h0)
+        Steering = Dense(1,activation='tanh')(h1)  
+        Acceleration = Dense(1,activation='sigmoid')(h1)   
+        Brake = Dense(1,activation='sigmoid')(h1) 
+
+        V = concatenate([Steering,Acceleration,Brake]) 
+      
+        model = Model(input=S,output=V)
+        print("--------------------------  I am in actor network ------------------------")
+        print("--------------------------  I am in actor network ------------------------")
+        return model, model.trainable_weights, S
+
